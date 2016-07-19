b1016862fe101207323b688d3f58997a604ab31d
sNiCKY <mpruszynski@gmail.com>
2016-07-19 23:43:23 +0200

    add a test for variables

--- test/simple_test.rb
+++ test/simple_test.rb
@@ -5,9 +5,9 @@ require_relative '../simple'
 
 class SimpleTest < Minitest::Test
 
-  def check_vm_output(expression, expected_output)
+  def check_vm_output(expression, environment, expected_output)
     assert_output(expected_output.strip_tabs) do
-      machine = Machine.new(expression)
+      machine = Machine.new(expression, environment)
       machine.run
     end
   end
@@ -23,7 +23,7 @@ class SimpleTest < Minitest::Test
         Number.new(4)
       )
     )
-    check_vm_output(expression, %(
+    check_vm_output(expression, nil, %(
       1 * 2 + 3 * 4
       2 + 3 * 4
       2 + 12
@@ -39,10 +39,27 @@ class SimpleTest < Minitest::Test
         Number.new(2)
       )
     )
-    check_vm_output(expression, %(
+    check_vm_output(expression, nil,  %(
       5 < 2 + 2
       5 < 4
       false
+    ))
+  end
+
+  def test_variable
+    expression = Add.new(
+      Variable.new(:x),
+      Variable.new(:y),
+    )
+    environment = {
+      x: Number.new(3),
+      y: Number.new(4)
+    }
+    check_vm_output(expression, environment,  %(
+      x + y
+      3 + y
+      3 + 4
+      7
     ))
   end
 
